1c0c8a667353109c897afb919fd72600438f21f7
richard <r50206v@gmail.com>
2021-01-08 10:02:27 -0800

    [FEATURE] adding new practice scripts

---
+++ January2021-Challenges/1506-FindRootOfN-AryTree.py
@@ -0,0 +1,49 @@
+'''
+time: O(N * max(c))
+c: is the amount of children nodes
+space: O(N)
+'''
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+class Solution:
+    def findRoot(self, tree: List['Node']) -> 'Node':
+        indegreeCount = collections.Counter()
+        
+        for t in tree:
+            if t not in indegreeCount:
+                indegreeCount[t] = 0
+            
+            if t.children:
+                for c in t.children:
+                    indegreeCount[c] += 1
+        
+        return indegreeCount.most_common()[-1][0]
+        
+
+
+'''
+time: O(N)
+space: O(1)
+'''
+class Solution:
+    def findRoot(self, tree: List['Node']) -> 'Node':
+        value_sum = 0
+
+        for node in tree:
+            # the value is added as a parent node
+            value_sum += node.val
+            for child in node.children:
+                # the value is deducted as a child node.
+                value_sum -= child.val
+
+        # the value of the root node is `value_sum`
+        for node in tree:
+            if node.val == value_sum:
+                return node
